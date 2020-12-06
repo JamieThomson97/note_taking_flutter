4a75986b80a52439c6783830f76536e06c38f58c
Jamie Thomson <jaytadmn@gmail.com>
2020-12-06 20:49:40 +0000

    trying out dartz stuff

---
+++ lib/domain/auth/email_address.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:note_taking_flutter/domain/core/failures.dart';
+import 'package:note_taking_flutter/domain/core/value_objects.dart';
+import 'package:note_taking_flutter/domain/core/value_validators.dart';
+
+@immutable
+class EmailAddress extends ValueObject<String> {
+
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      ValueValidators.validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+
+  // toString, equals, hashCode...
+}
---
+++ lib/domain/auth/password.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:note_taking_flutter/domain/core/failures.dart';
+import 'package:note_taking_flutter/domain/core/value_objects.dart';
+import 'package:note_taking_flutter/domain/core/value_validators.dart';
+
+class Password extends ValueObject<String>{
+
+  final Either<ValueFailure<String>, String> value;
+  
+  factory Password(String input){
+    assert (input != null);
+    return Password._(
+      ValueValidators.validatePassword(input)
+    );
+  }
+
+  const Password._(this.value);
+}
