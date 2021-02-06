344f1360d9850d1e40fe5cb14143b153277b7e76
Jamie Thomson <jaytadmn@gmail.com>
2021-02-06 15:24:27 +0000

    setup home page scroll view

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -6,8 +6,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(child: Text("Home Page")),
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    double titleBarProportion() => screenHeight * 0.08;
+    double carouselProportion() => screenHeight * 0.24;
+    double tileProportion() => screenHeight * 0.21;
+
+    final Container listSpacer = Container(
+      height: screenHeight * 0.02,
     );
+
+    return Material(
+        child: ListView(
+      padding: const EdgeInsets.fromLTRB(16, 45, 16, 0),
+      children: <Widget>[
+        Container(
+          height: titleBarProportion(),
+          color: Colors.amber[800],
+          child: const Center(child: Text('Title bar')),
+        ),
+        listSpacer,
+        Container(
+          height: carouselProportion(),
+          color: Colors.amber[700],
+          child: const Center(child: Text('Discover Carousel')),
+        ),
+        listSpacer,
+        Container(
+          height: tileProportion(),
+          color: Colors.amber[600],
+          child: const Center(child: Text('Recently Listened')),
+        ),
+        listSpacer,
+        Container(
+          height: tileProportion(),
+          color: Colors.amber[500],
+          child: const Center(child: Text('What we think you\'ll liked')),
+        ),
+        listSpacer,
+        Container(
+          height: tileProportion(),
+          color: Colors.amber[400],
+          child: const Center(child: Text('Dope producers')),
+        ),
+      ],
+    ));
   }
 }
